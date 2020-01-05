2a219052b21640ac3866bc7f107b4045b62f7552
Tharindu Wathukara <wtgwathnkara.15@cse.mrt.ac.lk>
2020-01-05 15:03:59 +0530

    join request ok

--- node/Neighbour.java
+++ node/Neighbour.java
@@ -12,20 +12,14 @@ package node;
 class Neighbour {
 	private String ip;
 	private int port;
-	private String username;
 
-	public Neighbour(String ip, int port, String username) {
+	public Neighbour(String ip, int port) {
 		this.ip = ip;
 		this.port = port;
-		this.username = username;
 	}
 
 	public String getIp() {
 		return this.ip;
-	}
-
-	public String getUsername() {
-		return this.username;
 	}
 
 	public int getPort() {
--- node/Node.java
+++ node/Node.java
@@ -15,6 +15,8 @@ import java.util.List;
 import java.util.Random;
 import java.util.StringTokenizer;
 
+import javafx.scene.effect.Light.Spot;
+
 /**
  *
  * @author Tharindu Wathukara
@@ -29,15 +31,15 @@ public class Node {
         DatagramSocket sock = null;
         InetAddress ip;
         int port = Integer.parseInt(args[0]);
-        ;
         String username = args[1];
         List<Neighbour> nodes = new ArrayList<Neighbour>();
+        String s;
 
         try {
             ip = InetAddress.getByName("localhost");
             sock = new DatagramSocket(port);
 
-            echo(username + " created at port " + port + ". Try to register at Bootstrap server....");
+            echo("Try to register at Bootstrap server....");
 
             String req = " REG " + ip.getHostAddress() + " " + port + " " + username;
             req = String.format("%04d", req.length() + 5) + " " + req;
@@ -45,28 +47,71 @@ public class Node {
             DatagramPacket request = new DatagramPacket(req.getBytes(), req.getBytes().length, ip, 55555);
             sock.send(request);
 
-            byte[] buffer = new byte[65536];
-            DatagramPacket ackForReg = new DatagramPacket(buffer, buffer.length);
-            sock.receive(ackForReg);
+            while (true) {
+                byte[] buffer = new byte[65536];
+                DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+                sock.receive(incoming);
 
-            byte[] data = ackForReg.getData();
-            String s = new String(data, 0, ackForReg.getLength());
-            // echo the details of incoming data - client ip : client port - client message
-            echo(ackForReg.getAddress().getHostAddress() + " : " + ackForReg.getPort() + " - " + s);
+                byte[] data = incoming.getData();
+                s = new String(data, 0, incoming.getLength());
 
-            StringTokenizer st = new StringTokenizer(s, " ");
-            String length = st.nextToken();
-            String command = st.nextToken();
-            String errorCode = st.nextToken();
+                // echo the details of incoming data - client ip : client port - client message
+                echo(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
 
-            if (command.equals("REGOK")) {
-                if (errorCode.equals("0")) {
-                    echo("REGISTER SUCCESSFULL");
-                } else if (errorCode.equals("9999")) {
-                    echo("REGISTER FAIL :: due to node unreachable");
-                } else if (errorCode.equals("9998")) {
-                    echo("REGISTER FAIL :: some other error");
+                StringTokenizer st = new StringTokenizer(s, " ");
+
+                String length = st.nextToken();
+                String command = st.nextToken();
+
+                if (command.equals("REGOK")) {
+                    String errorCode = st.nextToken();
+                    if (errorCode.equals("9999")) {
+                        echo("REGISTER FAIL :: due to node unreachable");
+                    } else if (errorCode.equals("9998")) {
+                        echo("REGISTER FAIL :: some other error");
+                    } else {
+                        echo("REGISTER SUCCESSFULL");
+                        for (int i = 0; i < Integer.parseInt(errorCode); i++) {
+                            String neighborIp = st.nextToken();
+                            int neighborPort = Integer.parseInt(st.nextToken());
+
+                            // send join request to neighbor node
+                            String joinReq = " JOIN " + neighborIp + " " + neighborPort;
+                            joinReq = String.format("%04d", joinReq.length() + 5) + " " + joinReq;
+
+                            DatagramPacket joinRequest = new DatagramPacket(joinReq.getBytes(),
+                                    joinReq.getBytes().length, InetAddress.getByName(neighborIp), neighborPort);
+                            sock.send(joinRequest);
+                        }
+
+                        echo(username + " created at port " + port + ". Waiting for incoming data...");
+                    }
+                } else if (command.equals("ECHO")) {
+                    for (int i = 0; i < nodes.size(); i++) {
+                        echo(nodes.get(i).getIp() + " " + nodes.get(i).getPort());
+                    }
+                    String reply = "0012 ECHOK 0";
+                    DatagramPacket dpReply = new DatagramPacket(reply.getBytes(), reply.getBytes().length,
+                            incoming.getAddress(), incoming.getPort());
+                    sock.send(dpReply);
+
+                } else if (command.equals("JOIN")) {
+                    String neighborIp = st.nextToken();
+                    int neighborPort = Integer.parseInt(st.nextToken());
+                    nodes.add(new Neighbour(neighborIp, neighborPort));
+                    echo("" + nodes.size());
+
+                    String reply = "0012 JOINOK 0";
+                    DatagramPacket dpReply = new DatagramPacket(reply.getBytes(), reply.getBytes().length,
+                            incoming.getAddress(), incoming.getPort());
+                    sock.send(dpReply);
                 }
+
+                else if (command.equals("JOINOK")) {
+                    nodes.add(new Neighbour(incoming.getAddress().getHostAddress(), incoming.getPort()));
+                    echo("" + nodes.size());
+                }
+
             }
         } catch (IOException e) {
             System.err.println("IOException " + e);
