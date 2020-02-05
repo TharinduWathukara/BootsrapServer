2518e15136bbb7e997cd070c6d5f9252baa0f77c
Tharindu Wathukara <wtgwathnkara.15@cse.mrt.ac.lk>
2020-02-05 22:13:42 +0530

    upto search

---
+++ bs/Neighbour.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bs;
+
+/**
+ *
+ * @author Tharindu Wathukara
+ */
+class Neighbour {
+	private String ip;
+	private int port;
+	private String username;
+
+	public Neighbour(String ip, int port, String username) {
+		this.ip = ip;
+		this.port = port;
+		this.username = username;
+	}
+
+	public String getIp() {
+		return this.ip;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public int getPort() {
+		return this.port;
+	}
+}
